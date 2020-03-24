3e32046de2301aab56edeaba11dff6fa93c044f9
shmoon <msk2292@naver.com>
2020-03-25 01:15:52 +0900

    function art one 신기하내... 메소드 자체 기능이 신기하다.. 제네릭 선언도 사용할 함수에 포커스가 맞춰져 있어 좋아보임

--- main.go
+++ main.go
@@ -2,16 +2,56 @@ package main
 
 import (
 	"fmt"
-	"github.com/shmoon0814/learngo/something"
+	"strings"
 )
 
-func main() {
-	fmt.Println("Hello World!")
-	something.SayHello()
+func multiply_1(a, b int) int { //받는 상수가 같은자료형이면 이렇게 묶어라.
+	return a * b
+}
 
-	// Println 이 대문자로 실행되는 이유는
-	// go 에선 Uppercase method 는 public
-	// lowercase method 는 private
-	// main에서는 helloWorld를 찾을 수 없지만 something 패키지 내부에서는 호출 할 수 있다.
-	// 대부분의 호출 되는 메소드 들은 대문자로 시작 할 것
+func multiply_2(a int, b int) int { //받는 변수가 다르면 따로선언하시던가요
+	return a * b
+}
+
+//리턴을 여러개 할 수도 있다. 이거 좀 좋냉...
+func checkLengthAndWord(words string) (int, string) {
+	return len(words), strings.ToUpper(words)
+}
+
+func sayWord(words ...string) {
+	fmt.Println(words)
+}
+
+//이건 설명이 안나와있는디.. 내가 배열 받으니 배열 넘기는거겠
+func returnWord(words ...string) int {
+	return len(words)
+}
+
+func main() {
+
+	fmt.Println(multiply_1(4, 2))
+	fmt.Println(multiply_2(13, 4))
+	fmt.Println(checkLengthAndWord("상혁아..."))
+
+	//리턴 두개 받는법
+	leng, word := checkLengthAndWord("상혁아")
+	fmt.Println(leng)
+	fmt.Println(word)
+
+	//두개중에 하나만 받고싶을때. _ 로 하면 변수 블랭크 컴파일러가 무시한다
+	leng_2, _ := checkLengthAndWord("상혁아")
+	fmt.Println(leng_2)
+
+	_, word_2 := checkLengthAndWord("가나다라")
+	fmt.Println(word_2)
+
+	//for문 돌릴 꺼 이렇게 변수 ...로 선언해 놓으면 한기능 반복처리가 가능하다.
+	//아 이거 혁명인디?? 나중에 포문을 좀 봐야겠는데.. 지
+	//뭐 결국 파라미터 인자값을 넘기는걸 좀더 편히 할 수 있다 정도다.
+	//특정 연산 처리 후 넘기는 자료형 정리가 된다면 좋긴 하겠다.
+	sayWord("하나")
+	sayWord("하나", "둘", "셋", "넷")
+
+	arrayLeng := returnWord("하나", "둘", "셋", "넷", "다섯")
+	fmt.Println(arrayLeng)
 }
