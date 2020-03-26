c9192a61555d82fae3143d44a56ed2f1db9bfde8
shmoon <msk2292@naver.com>
2020-03-27 00:10:07 +0900

    pointer 의 다양한 표현법과 뭐시기 뭐시기
    메모리 주소를 직접 바꿔주면 뭔가 빠르다고 함
    실정 코드때 써먹어 봐야할듯..

--- main.go
+++ main.go
@@ -2,28 +2,23 @@ package main
 
 import "fmt"
 
-func canIDrink(age int) bool {
-	switch {
-	case age < 18:
-		return false
-	case age == 18:
-		return true
-	}
-	return true
-}
-
-func canIDrinkKorea(age int) bool {
-	switch koreanAge := age + 2; {
-	case koreanAge < 18:
-		return false
-	case koreanAge == 18:
-		return true
-	}
-	return true
-}
-
 func main() {
+	a := 2
+	b := a
+	fmt.Println(&a, &b)
+	//주소 확인
 
-	fmt.Println(canIDrink(16))
-	fmt.Println(canIDrinkKorea(16))
+	c := 4
+	d := &c //주소값을 저장하는 법
+	fmt.Println(&c, d)
+	fmt.Println(*d) //여기서 d가 왜 4가 나오냐면
+	//자동타입 추론으로 인하여 &가 달렸으면 주소를 나타내는 자료형이라서
+	//d는 메모리 주소를 나타내는 자료형이고 *는 메모리 자료형의 주소의 값을 나타내는 것
+
+	e := 3
+	f := &e
+	*f = 20
+	fmt.Println(e, &e)
+	//주소값을 가지고 해당 값을 변경할 수도 있다.
+
 }
